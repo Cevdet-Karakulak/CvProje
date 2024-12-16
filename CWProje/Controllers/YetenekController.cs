702f9ea763bf81e70685462926684bc36c5e0f7b
C3vd3t <63650334+cevdetkarakulak@users.noreply.github.com>
2024-12-16 17:19:59 +0300

    Update

---
+++ CWProje/Controllers/YetenekController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CWProje.Models.Entity;
+using CWProje.Repositories;
+
+namespace CWProje.Controllers
+{
+    public class YetenekController : Controller
+    {
+        // GET: Yetenek
+        GenericRepository<Yeteneklerim> repo=new GenericRepository<Yeteneklerim>();
+        public ActionResult Index()
+        {
+            var yetenekler = repo.List();
+            return View(yetenekler);
+        }
+        [HttpGet]
+        public ActionResult YeniYetenek ()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniYetenek(Yeteneklerim p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult YetenekSil(int id)
+        {
+           var yetenek=repo.Find(x=>x.ID==id);
+            repo.Tdelete(yetenek);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult YetenekDuzenle(int id) 
+        {
+            var yetenek=repo.Find(x=>x.ID == id);
+            return View(yetenek);
+        }
+        [HttpPost]
+        public ActionResult YetenekDuzenle(Yeteneklerim t)
+        {
+            var y=repo.Find(x=>x.ID==t.ID);
+            y.Yetenek=t.Yetenek;
+            y.Oran = t.Oran;
+            repo.Tupdate(y);
+            return RedirectToAction("Index");
+        }
+    }
+}
